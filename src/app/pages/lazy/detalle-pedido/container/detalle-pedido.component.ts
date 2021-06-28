9164dc3cc82de6de35d4b1e130cf35f9f21c7ac9
Nicolas Magliano <nicolasmagliano@hotmail.com>
2021-06-28 19:23:44 -0300

    llamada al servicio de productos

--- src/app/pages/lazy/detalle-pedido/container/detalle-pedido.component.ts
+++ src/app/pages/lazy/detalle-pedido/container/detalle-pedido.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs/operators';
+import { ProductService } from 'src/app/core/services/products.service';
 
 @Component({
     templateUrl: 'detalle-pedido.component.html'
 })
 
 export class DetallePedidoComponent implements OnInit {
-    constructor() { }
 
-    ngOnInit() { }
+    public products: any[] = [];
+
+    constructor(private productServices: ProductService) { }
+
+    ngOnInit(): void {
+        this.productServices
+          .get()
+          .pipe(take(1))
+          .subscribe((apiResponse) => {
+            this.products = apiResponse.data;
+          });
+      }
 }
